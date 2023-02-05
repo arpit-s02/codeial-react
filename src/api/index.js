84d597630f2395416cd25e23c6c5dcc2962333c5
arpit-s02 <arpitrahul02@gmail.com>
2023-02-05 16:00:22 +0530

    set up customFetch function and fetch posts from the API

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,62 @@
-const customFetch = () => {};
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from '../utils';
 
-const getPosts = (page, limit) => {
-  return customFetch();
+// in the second argument, body is destructured and rest of the keys are called customConfig
+const customFetch = async (url, { body, ...customConfig }) => {
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+  const headers = {
+    'content-type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  // if token exists, we are adding authorization key to headers as some APIs require authorization
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  // creating config to be passed in fetch
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  // stringifying the body as we cannnot send objects directly, and adding it inside the config
+  // body can contain keys such as email and password in log-in
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    // fetching the data from the API
+    const response = await fetch(url, config);
+
+    // converting the response into JSON
+    const data = await response.json();
+
+    if (data.success) {
+      // if the data received has success == true
+      return {
+        data: data.data,
+        success: true,
+      };
+    }
+
+    // else we'll throw error
+    throw new Error(data.message);
+  } catch (error) {
+    console.error('error');
+    return {
+      message: error.message,
+      success: false,
+    };
+  }
+};
+
+export const getPosts = (page = 1, limit = 5) => {
+  return customFetch(API_URLS.posts(page, limit), {
+    method: 'GET',
+  });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { getPosts } from '../api';
+
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsResponse = await getPosts();
+      console.log('postsResponse', postsResponse);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <h1> Hello World </h1>
