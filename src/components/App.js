dee5c957e905ef73912551925427534beec15124
arpit-s02 <arpitrahul02@gmail.com>
2023-02-05 22:57:23 +0530

    render the posts received from the API

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
 import { Home } from '../pages';
+import Loader from './loader';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    // fetching posts from the API
     const fetchPosts = async () => {
       const postsResponse = await getPosts();
-      console.log('postsResponse', postsResponse);
+
+      if (postsResponse.success) {
+        // if posts are successfully fetched we set the state of posts as the posts received
+        setPosts(postsResponse.data.posts);
+
+        // when posts are received we set loading as false
+        setLoading(false);
+      }
     };
 
     fetchPosts();
   }, []);
 
+  if (loading) {
+    // while posts are being fetched we display the loader instead of home component
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <Home />
+      <Home posts={posts} />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,71 @@
 import styles from '../styles/home.module.css';
 
-export const Home = () => {
+export const Home = ({ posts }) => {
   return (
-    <div>
-      <div className={styles.postContainer}>
-        <div className={styles.postUserInfoContainer}>
-          <div className={styles.postUserDP}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png"
-              alt="User dp"
-            />
-          </div>
-          <div className={styles.postUserInfo}>
-            <span>
-              {' '}
-              <b> Arpit </b>{' '}
-            </span>
-            <span className={styles.postTime}> a minute ago </span>
-          </div>
-        </div>
-        <div className={styles.postContentContainer}>
-          <div className={styles.postContent}>
-            <h1> Post Content </h1>
-          </div>
-          <div className={styles.postLikesComments}>
-            <button className={styles.button}>
+    <div className={styles.homeContainer}>
+      {/* rendering posts one by one */}
+      {posts.map((post, index) => (
+        <div className={styles.postContainer} key={index}>
+          <div className={styles.postUserInfoContainer}>
+            <div className={styles.postUserDP}>
               <img
-                src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                alt="Like Button"
+                src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png"
+                alt="User dp"
               />
-            </button>
-            <span className={styles.numLikes}> 5 </span>
-            <button className={styles.button}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/3193/3193015.png"
-                alt="Like Button"
-              />
-            </button>
-            <span className={styles.numComments}> 2 </span>
-          </div>
-        </div>
-        <div className={styles.commentsContainer}>
-          <div>
-            <textarea
-              name="comment"
-              placeholder="Add a comment..."
-              className={styles.addComment}
-            ></textarea>
-          </div>
-          <div className={styles.prevComments}>
-            <div className={styles.commentUserInfo}>
-              <span className={styles.userName}> Bill </span>
-              <span className={styles.commentTime}> a minute ago </span>
             </div>
-            <div className={styles.comment}>
-              <span>Random Comment</span>
+            <div className={styles.postUserInfo}>
+              <span>
+                {' '}
+                <b> {post.user.name} </b>{' '}
+              </span>
+              <span className={styles.postTime}> a minute ago </span>
             </div>
           </div>
+          <div className={styles.postContentContainer}>
+            <div className={styles.postContent}>
+              <h1> {post.content} </h1>
+            </div>
+            <div className={styles.postLikesComments}>
+              <button className={styles.button}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                  alt="Like Button"
+                />
+              </button>
+              <span className={styles.numLikes}> {post.likes.length} </span>
+              <button className={styles.button}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/3193/3193015.png"
+                  alt="Like Button"
+                />
+              </button>
+              <span className={styles.numComments}> 2 </span>
+            </div>
+          </div>
+          <div className={styles.commentsContainer}>
+            <div>
+              <textarea
+                name="comment"
+                placeholder="Add a comment..."
+                className={styles.addComment}
+              ></textarea>
+            </div>
+
+            {/* rendering comments one by one in each post */}
+            {post.comments.map((comment, index) => (
+              <div className={styles.prevComments} key={index}>
+                <div className={styles.commentUserInfo}>
+                  <span className={styles.userName}> {comment.user.name} </span>
+                  <span className={styles.commentTime}> a minute ago </span>
+                </div>
+                <div className={styles.comment}>
+                  <span>{comment.content}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
