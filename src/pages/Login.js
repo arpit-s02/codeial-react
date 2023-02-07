9c39fccbab9e332bc4f09d7c5a7a9105bd352cfb
arpit-s02 <arpitrahul02@gmail.com>
2023-02-07 21:05:53 +0530

    create login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,29 @@
+import styles from '../styles/logInSignUp.module.css';
+
 export const Login = () => {
   return (
-    <div>
-      <h1> Login </h1>
+    <div className={styles.logInSignUpBackground}>
+      <div className={styles.logInSignUpContainer}>
+        <h1> Log In </h1>
+        <div className={styles.logInSignUpForm}>
+          <form>
+            <div className={styles.logInSignUpField}>
+              <input type="email" placeholder="Email" name="email" required />
+            </div>
+
+            <div className={styles.logInSignUpField}>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+              />
+            </div>
+
+            <button className={styles.submitButton}> Log In </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
